9a1b546c9053cbe51ab9e286ad608ece024b7312
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-11-24 22:14:50 +0100

    Update program.cpp

--- src/program.cpp
+++ src/program.cpp
@@ -3,13 +3,13 @@
 
 int MainThread(int argc, void* argv) {
 	int title_id = OSGetTitleID();
-	if(title_id != 0x00050000101d7500) //Minecraft Title ID
+	if(title_id != 0x00050000101d7500) // Minecraft EUR Title ID
 	{
-		log_printf("ERROR : Program can't starting games with codes !\n");
+		log_printf("Error: can't starting the game with codes.\n");
 		return 0;
 	}
 
-	//Your codes
+	// -- Your codes here --
 
 	return 0;
 }
